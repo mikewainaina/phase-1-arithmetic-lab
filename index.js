819291cf76c56e49a68db382544759786448ffe2
Michael <michael.wainaina@student.moringaschool.com>
2024-03-27 15:13:44 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+let multiply
+let num1 = 2
+let num2 = 31
+multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1
+let mod
+let num3 = 20
+let num4 = 5
+mod = num3 / num4
+let max
+const mySet = new Set([10, 5, 20, 2])
+max = Math.max(...mySet)
